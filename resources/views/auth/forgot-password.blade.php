d4c3ba5b1cf21bef33287d9cb200e327941f9c80
EC2 Default User <ec2-user@ip-172-31-16-255.ap-southeast-2.compute.internal>
2024-05-01 05:01:50 +0000

    パスワードリセット画面、メールを日本語化

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -29,6 +29,11 @@
                     {{ __('Email Password Reset Link') }}
                 </x-button>
             </div>
+            <div class="flex items-center justify-end mt-4">
+                <a class="block underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                    ログイン画面に戻る
+                </a>
+            </div>
         </form>
     </x-authentication-card>
 </x-guest-layout>
