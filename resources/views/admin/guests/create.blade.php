3390b606c49438c06f29c172f6d2b445baea37bf
EC2 Default User <ec2-user@ip-172-31-16-255.ap-southeast-2.compute.internal>
2024-05-14 08:09:19 +0000

    個人情報チェックを管理画面でもhiddenで渡す

--- resources/views/admin/guests/create.blade.php
+++ resources/views/admin/guests/create.blade.php
@@ -24,15 +24,16 @@ $selected = (old('times') == $time) ? ' selected="selected"' : '';
 @section('checkbox')
     <div class="mt-8">
         <div class="flex gap-2 md:gap-3">
-            <input type="checkbox" name="registCheck" class="mt-1 border-gray-400 rounded cursor-pointer" id="registCheck"@if(old('registCheck'))' checked="checked"@endif />
-            <label for="registCheck" class="font-normal text-sm leading-6">
-                  定員オーバーでも登録可
+@php
+$checked = (old('registCheck')) ? ' checked="checked"' : '';
+@endphp
+            <input type="checkbox" name="registCheck" class="mt-1 border-gray-400 rounded cursor-pointer" id="registCheck"{{ $checked }} />
+            <label for="registCheck" class="font-normal text-sm">
+                定員オーバーでも登録可
             </label>
         </div>
-@error('registCheck')
-        <p class="mt-2 text-red-500 text-xs">※{{ $message }}</p>
-@enderror
     </div>
+    <input type="hidden" name="agreeCheck" value="on" />
 @endsection
 
     @include('common.guests.form', ['route' => route('admin.guests.store'), 'submitText' => '登録', 'formAddClass' => 'md:w-1/3'])
